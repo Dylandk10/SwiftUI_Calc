e548c4bcac23a62e7a46fbaabb74ffcbd5f7edef
Dylan Kelly <dylankelly@dylans-mbp.myfiosgateway.com>
2021-09-05 16:18:26 -0400

    added button and label design

--- TestUI/ContentView.swift
+++ TestUI/ContentView.swift
@@ -11,50 +11,153 @@ let mHandler = MathHandler()
 struct ContentView: View {
     @State var UIText:String = mHandler.handleUIText()
     var body: some View {
-        VStack {
+        VStack(spacing:10) {
             //main text for result and input id=mainLabel
             HStack {
                 Text(UIText)
                     .font(.largeTitle)
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .padding(20)
+                    .padding(40)
+                    .frame(maxWidth: .infinity)
                     .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    .id("mainLabel")
+            }
+            HStack {
+                //clear button
+                Button("CLS") {
+                
+                }.font(.title).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.red)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                Spacer()
+                
+                Button("=") {
+                    
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.green)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            
             }
             
-            HStack {
+            HStack(spacing:5) {
                 
                 Button("0") {
                     mHandler.concatNumber(num: "0")
                     UIText = mHandler.handleUIText()
-                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                 
                 Button("1") {
                     mHandler.concatNumber(num: "1")
                     UIText = mHandler.handleUIText()
-                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                 
                 Button("2") {
                     mHandler.concatNumber(num: "2")
                     UIText = mHandler.handleUIText()
-                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                Button("+") {
+
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.orange)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             }
             
-            HStack {
+            HStack(spacing:5) {
                 Button("3") {
                     mHandler.concatNumber(num: "3")
                     UIText = mHandler.handleUIText()
-                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                 
                 Button("4") {
                     mHandler.concatNumber(num: "4")
                     UIText = mHandler.handleUIText()
-                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                 
                 Button("5") {
                     mHandler.concatNumber(num: "5")
                     UIText = mHandler.handleUIText()
-                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                Button("-") {
+                    
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.orange)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            }
+            
+            HStack(spacing:5) {
+                Button("6") {
+                    mHandler.concatNumber(num: "6")
+                    UIText = mHandler.handleUIText()
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                Button("7") {
+                    mHandler.concatNumber(num: "7")
+                    UIText = mHandler.handleUIText()
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                Button("7") {
+                    mHandler.concatNumber(num: "8")
+                    UIText = mHandler.handleUIText()
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                
+                Button("/") {
+
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.orange)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            }
+            
+            HStack {
+                Button("9") {
+                    mHandler.concatNumber(num: "9")
+                    UIText = mHandler.handleUIText()
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.gray)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+
+                Button("x") {
+
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).frame(width:100, height:100)
+                .foregroundColor(.white)
+                .background(Color.orange)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .offset(x:100)
+                
             }
         }
     }
